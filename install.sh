f49bdc906f93c0fc66c345c90a9dcfd501645d49
‘puriso’ <nrs.yuuki@gmail.com>
2016-01-27 18:51:56 +0900

    Mod

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-ln -sf ~/.vimrc ~/git/dot/.vimrc
-ln -sf ~/.jshintrc ~/dot/.jshintrc
-
+ln -sf ~/git/dot/.vimrc ~/.vimrc
+ln -sf ~/git/dot/.jshintrc ~/.jshintrc
+ln -sf ~/git/dot/.zshrc ~/.zshrc
 
 
 echo "end!"
