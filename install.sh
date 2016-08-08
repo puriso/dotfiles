5ceccf05718be5db73543c451a8f0a1d340fafc1
‘puriso’ <nrs.yuuki@gmail.com>
2016-08-08 14:40:12 +0900

    Mod

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-#Install NeoBundle
+echo "start installing..."
+
+# Install NeoBundle
 mkdir -p ~/.vim/bundle
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
-#Make symbolic link for dotfiles
+# Make symbolic link for dotfiles
 ln -sf ~/.vimrc ~/git/dot/.vimrc
 ln -sf ~/.jshintrc ~/dot/.jshintrc
 
+# Install ctags
 
 
-echo "end!"
+echo "...end!"
