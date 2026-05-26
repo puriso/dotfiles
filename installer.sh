b82b247c1c8440ade82f6cef7a34835055f7553a
purin <puriso@users.noreply.github.com>
2026-05-27 03:14:41 +0900

    fix: resolve critical shell script failures

--- installer.sh
+++ installer.sh
@@ -1,18 +1,21 @@
-#!/bin/sh
-# Standalone installer for Unixs
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Standalone installer for Unix系OS
 # Original version is created by shoma2da
 # https://github.com/shoma2da/neobundle_installer
 
-if [ $# -ne 1 ]; then
-  echo "You must specify the installation directory!"
+if [ "$#" -ne 1 ]; then
+  echo "インストール先ディレクトリを1つ指定してください。"
   exit 1
 fi
 
-# Convert the installation directory to absolute path
-case $1 in
-  /*) PLUGIN_DIR=$1;;
-  *) PLUGIN_DIR=$PWD/$1;;
+# インストール先を絶対パスに変換
+case "$1" in
+  /*) PLUGIN_DIR="$1" ;;
+  *) PLUGIN_DIR="$PWD/$1" ;;
 esac
+
 INSTALL_DIR="${PLUGIN_DIR}/repos/github.com/Shougo/dein.vim"
 echo "Install to \"$INSTALL_DIR\"..."
 if [ -e "$INSTALL_DIR" ]; then
@@ -21,15 +24,15 @@ fi
 
 echo ""
 
-# check git command
-type git || {
+# gitコマンドの存在確認
+if ! command -v git >/dev/null 2>&1; then
   echo 'Please install git or update your path to include the git executable!'
   exit 1
-}
+fi
 echo ""
 
-# make plugin dir and fetch dein
-if ! [ -e "$INSTALL_DIR" ]; then
+# プラグインディレクトリを作成してdeinを取得
+if [ ! -e "$INSTALL_DIR" ]; then
   echo "Begin fetching dein..."
   mkdir -p "$PLUGIN_DIR"
   git clone https://github.com/Shougo/dein.vim "$INSTALL_DIR"
@@ -37,53 +40,50 @@ if ! [ -e "$INSTALL_DIR" ]; then
   echo ""
 fi
 
-# write initial setting for .vimrc
+# 初期設定を出力
 echo "Please add the following settings for dein to the top of your vimrc (Vim) or init.vim (NeoVim) file:"
-{
-    echo ""
-    echo ""
-    echo "\"dein Scripts-----------------------------"
-    echo "if &compatible"
-    echo "  set nocompatible               \" Be iMproved"
-    echo "endif"
-    echo ""
-    echo "\" Required:"
-    echo "set runtimepath+=$INSTALL_DIR"
-    echo ""
-    echo "\" Required:"
-    echo "if dein#load_state('$PLUGIN_DIR')"
-    echo "  call dein#begin('$PLUGIN_DIR')"
-    echo ""
-    echo "  \" Let dein manage dein"
-    echo "  \" Required:"
-    echo "  call dein#add('$INSTALL_DIR')"
-    echo ""
-    echo "  \" Add or remove your plugins here:"
-    echo "  call dein#add('Shougo/neosnippet.vim')"
-    echo "  call dein#add('Shougo/neosnippet-snippets')"
-    echo ""
-    echo "  \" You can specify revision/branch/tag."
-    echo "  call dein#add('Shougo/deol.nvim', { 'rev': '01203d4c9' })"
-    echo ""
-    echo "  \" Required:"
-    echo "  call dein#end()"
-    echo "  call dein#save_state()"
-    echo "endif"
-    echo ""
-    echo "\" Required:"
-    echo "filetype plugin indent on"
-    echo "syntax enable"
-    echo ""
-    echo "\" If you want to install not installed plugins on startup."
-    echo "\"if dein#check_install()"
-    echo "\"  call dein#install()"
-    echo "\"endif"
-    echo ""
-    echo "\"End dein Scripts-------------------------"
-    echo ""
-    echo ""
-}
+cat <<SETTINGS
+
+"dein Scripts-----------------------------
+if &compatible
+  set nocompatible               " Be iMproved
+endif
+
+" Required:
+set runtimepath+=$INSTALL_DIR
+
+" Required:
+if dein#load_state('$PLUGIN_DIR')
+  call dein#begin('$PLUGIN_DIR')
+
+  " Let dein manage dein
+  " Required:
+  call dein#add('$INSTALL_DIR')
+
+  " Add or remove your plugins here:
+  call dein#add('Shougo/neosnippet.vim')
+  call dein#add('Shougo/neosnippet-snippets')
+
+  " You can specify revision/branch/tag.
+  call dein#add('Shougo/deol.nvim', { 'rev': '01203d4c9' })
+
+  " Required:
+  call dein#end()
+  call dein#save_state()
+endif
+
+" Required:
+filetype plugin indent on
+syntax enable
+
+" If you want to install not installed plugins on startup.
+"if dein#check_install()
+"  call dein#install()
+"endif
+
+"End dein Scripts-------------------------
+
+SETTINGS
 
 echo "Done."
-
 echo "Complete setup dein!"
